55ea870ff7c25b6767d7dc142f6adb7452003bd4
yucheng2 <38121997+yucheng2@users.noreply.github.com>
2023-07-20 22:06:51 +0800

    e

--- test.js
+++ test.js
@@ -42,24 +42,6 @@ const obj = {
     date: new Date(),
 }
 const res = generateMockTemplate(obj)
-console.log(res)
-const mockRes = Mock.mock({
-    'age|0-100': 0,
-    'name|5': '张三',
-    'isMale|1': true,
-    'arr|10': [
-        {
-            'name|5': '张三',
-            'age|0-100': 18,
-            'list|10': [{'name|5': '张三', 'age|0-100': 18}]
-        }
-    ],
-    'obj|1': {
-        'a|0-100': 1,
-        'obj|1': {'name|5': '张三', 'age|0-100': 18},
-        'list|10': {'name|5': '张三', 'age|0-100': 18}
-    },
-    'date|1': {}
-})
-const test = JSON.stringify(mockRes)
-fs.writeFileSync('./test.json', test)
+// const mockRes = Mock.mock(res)
+// const test = JSON.stringify(mockRes)
+fs.writeFileSync('./test.json', JSON.stringify(res))
